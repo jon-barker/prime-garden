780172bc0a495c63602ac4b239cb446359eb9590
Jon Barker <19699370+jon-barker@users.noreply.github.com>
2025-04-30 12:57:46 -0600

    exploit known prime forms

--- main.cpp
+++ main.cpp
@@ -4,11 +4,17 @@
 inline bool isPrime(int n) {
     if (n < 2) return false;
     if (n == 2) return true;
+    if (n == 3) return true;
     if ((n & 1) == 0) return false;  // equivalent to n % 2 == 0
 
-    // Only test odd divisors up to sqrt(n)
-    for (int i = 3; i * i <= n; i += 2) {
-        if (n % i == 0) return false;
+    // Check for divisibility by 3
+    if (n % 3 == 0) return false;
+
+    // Only test odd divisors of the form 6k+1 and 6k+5 up to sqrt(n)
+    // since all primes >5 have one of those forms
+    for (int i = 5; i <= n / i; i += 6) {
+    if (n % i == 0 || n % (i + 2) == 0)
+        return false;
     }
     return true;
 }
